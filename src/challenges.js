fa33751b7b0c616a23087a7ac56646d927bbbdc0
Breno L. Lopes <brernolopes@hotmail.com>
2022-08-02 16:28:07 -0300

    fix: corrigidos erros de lind do arquivo challenges

--- src/challenges.js
+++ src/challenges.js
@@ -1,131 +1,101 @@
 // Desafio 1
 function compareTrue(v1, v2) {
   if (v1 === true && v2 === true) {
-    return true
-  } else {
-    return false
+    return true;
   }
+  return false;
 }
 // Desafio 2
 function calcArea(base, height) {
   let resultado = 0;
   resultado = (base * height) / 2;
-  return resultado
+  return resultado;
 }
 // Desafio 3
 function splitSentence(frase) {
-  let splitString = frase.split(" ")
-  return splitString
+  let splitString = frase.split(' ');
+  return splitString;
 }
 // Desafio 4
 function concatName(name) {
-  let concat = name[name.length - 1] + ', ' + name[0]
-  return concat
+  let concat = `${name[name.length - 1]}, ${name[0]}`;
+  return concat;
 }
 // Desafio 5
 function footballPoints(wins, ties) {
-  let pointWin = wins * 3
-  let pointTies = ties
-  let points = pointWin + pointTies
-  return points
+  let pointWin = wins * 3;
+  let pointTies = ties;
+  let points = pointWin + pointTies;
+  return points;
 }
 // Desafio 6
 function highestCount(numbers) {
-  let highest = -100
-  let compare = 0
-  for (index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] > highest) {
-      highest = numbers[index] 
+  let highest = -100;
+  let compare = 0;
+  numbers.foreach((element) => {
+    if (element > highest) {
+      highest = element;
     }
-  }
-  for (equal = 0; equal < numbers.length; equal += 1) {
-    if (numbers[equal] === highest) {
-      compare = compare + 1
+  });
+  numbers.foreach((element) => {
+    if (element === highest) {
+      compare += 1;
     }
-  }
-  return compare
+  });
+  return compare;
 }
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let distanciaCat1 = mouse - cat1
-  let distanciaCat2 = mouse - cat2
-  if(distanciaCat1 < 0){
-    distanciaCat1 = distanciaCat1 * -1
+  let distanciaCat1 = mouse - cat1;
+  let distanciaCat2 = mouse - cat2;
+  if (distanciaCat1 < 0) {
+    distanciaCat1 *= -1;
   }
-  if(distanciaCat2 < 0){
-    distanciaCat2 = distanciaCat2 * -1
+  if (distanciaCat2 < 0) {
+    distanciaCat2 *= -1;
   }
-  if(distanciaCat1 < distanciaCat2){
-    return "cat1"
-  }else if(distanciaCat2 < distanciaCat1){
-    return "cat2"
-  }else if(distanciaCat1 == distanciaCat2){
-    return "os gatos trombam e o rato foge"
+  if (distanciaCat1 < distanciaCat2) {
+    return 'cat1';
   }
+  if (distanciaCat2 < distanciaCat1) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 // Desafio 8
 function fizzBuzz(param) {
-  let result = []
-  for(let index = 0; index < param.length; index +=1){
-    if(param[index] % 3 == false && param[index] % 5 == false){
-      result.push("fizzBuzz")
-    } else if(param[index] % 5 == false){
-      result.push("buzz")
-    }else if(param[index] % 3 == false){
-      result.push("fizz")
-    }else{
-      result.push("bug!")
+  let result = [];
+  param.foreach((element) => {
+    if (element % 3 === false && element % 5 === false) {
+      result.push('fizzBuzz');
     }
-}
-return result
+    if (element % 5 === false) {
+      result.push('buzz');
+    }
+    if (element % 3 === false) {
+      result.push('fizz');
+    }
+    result.push('bug!');
+  });
+
+  return result;
 }
 // Desafio 9
 function encode(codeString) {
-  let codado = codeString.split("")
-   for(index = 0; index < codado.length; index +=1){
-     switch(codado[index]){
-       case "a":
-         codado[index] = "1";
-         break
-       case "e":
-         codado[index] = "2"
-         break
-       case "i":
-         codado[index] = "3"
-         break
-       case "o":
-         codado[index] = "4"
-         break
-       case "u":
-         codado[index] = "5"
-         break     
-      }
-   }
-    return codado.join("")
-    
+  const codado = codeString.replaceAll('a', 1)
+    .replaceAll('e', 2)
+    .replaceAll('i', 3)
+    .replaceAll('o', 4)
+    .replaceAll('u', 5);
+  return codado;
 }
 function decode(dCode) {
-  let decodado = dCode.split("")
-  for(index = 0; index < decodado.length; index +=1){
-    switch(decodado[index]){
-      case "1":
-        decodado[index] = "a";
-        break
-      case "2":
-        decodado[index] = "e"
-        break
-      case "3":
-        decodado[index] = "i"
-        break
-      case "4":
-        decodado[index] = "o"
-        break
-      case "5":
-        decodado[index] = "u"
-        break     
-     }
-  }
-   return decodado.join("")
+  const codado = dCode.replaceAll(1, 'a')
+    .replaceAll(2, 'e')
+    .replaceAll(3, 'i')
+    .replaceAll(4, 'o')
+    .replaceAll(5, 'u');
+  return codado;
 }
 module.exports = {
   calcArea,
